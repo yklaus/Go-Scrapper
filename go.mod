1240ecb6b3cc36089f537e82e7bcbeb7879da29c
yklaus <hwchon1234@gmail.com>
2020-08-17 16:04:51 +0900

    Using a web framework that name is 'echo'

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/yklaus/Go-Scrapper
 
 go 1.14
 
-require github.com/PuerkitoBio/goquery v1.5.1
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/labstack/echo/v4 v4.1.16
+)
--- main.go
+++ main.go
@@ -1,149 +1,42 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/yklaus/Go-Scrapper/scrapper"
 	"log"
-	"net/http"
 	"os"
-	"strconv"
 	"strings"
+	"time"
 )
-import "github.com/PuerkitoBio/goquery"
 
-type extractedJob struct {
-	id       string
-	title    string
-	location string
-	salary   string
-	summary  string
-}
-
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+const fileName = "jobs.csv"
 
 func main() {
-	var jobs []extractedJob
-	totalPages := getPages()
+	// Echo instance
+	e := echo.New()
 
-	c := make(chan []extractedJob)
+	// Routes
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
 
-	for i := 0; i < totalPages; i++ {
-		go getPage(i, c)
-	}
-
-	for i := 0; i < totalPages; i++ {
-		extractedJobs := <-c
-		jobs = append(jobs, extractedJobs...)
-	}
-
-	writeJobs(jobs)
-	fmt.Println("Done extracted: ", len(jobs))
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func writeJobs(jobs []extractedJob) {
-	// file 만들기
-	file, err := os.Create("jobs.csv")
-	checkErr(err)
-
-	// csv 저장
-	w := csv.NewWriter(file)
-	// 함수가 끝나고 나면 실행될 명령
-	defer w.Flush()
-
-	headers := []string{"ID", "Title", "Location", "Salary", "Summary"}
-	wErr := w.Write(headers)
-	checkErr(wErr)
-
-	writeC := make(chan string)
-	for _, job := range jobs {
-		go writeRecord(&w, job, writeC)
-	}
-
-	for i := 0; i < len(jobs); i++ {
-		<- writeC
-	}
-
-	wErr2 := w.Error()
-	checkErr(wErr2)
+// Handler
+func handleHome(c echo.Context) error {
+	return c.File("index.html")
 }
 
-func writeRecord(w **csv.Writer, job extractedJob, c chan<- string) {
-	record := []string{job.id, job.title, job.location, job.salary, job.summary}
-	err := (*w).Write(record)
-	checkErr(err)
-	c <- "done"
-}
-
-func getPage(page int, jobsChannel chan<- [] extractedJob) {
-	var jobs []extractedJob
-	c := make(chan extractedJob)
-	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
-	fmt.Println("Requesting: ", pageURL)
-	res, err := http.Get(pageURL)
-	checkErr(err)
-	checkCode(res)
-
-	defer res.Body.Close()
-	doc, queryErr := goquery.NewDocumentFromReader(res.Body)
-	checkErr(queryErr)
-
-	searchCards := doc.Find(".jobsearch-SerpJobCard")
-	searchCards.Each(func(i int, card *goquery.Selection) {
-		go extractJob(card, c)
-	})
-
-	for i := 0; i < searchCards.Length(); i++ {
-		job := <-c
-		jobs = append(jobs, job)
-	}
-
-	jobsChannel <- jobs
-}
-
-func extractJob(card *goquery.Selection, c chan<- extractedJob) {
-	id, _ := card.Attr("data-jk")
-	title := cleanString(card.Find(".title>a").Text())
-	location := cleanString(card.Find(".sjcl").Text())
-	salary := cleanString(card.Find(".salaryText").Text())
-	summary := cleanString(card.Find(".summary").Text())
-
-	c <- extractedJob{
-		id:       id,
-		title:    title,
-		location: location,
-		salary:   salary,
-		summary:  summary,
-	}
-}
-
-func getPages() int {
-	pages := 0
-	res, err := http.Get(baseURL)
-	checkErr(err)
-	checkCode(res)
-	defer res.Body.Close()
-	doc, queryErr := goquery.NewDocumentFromReader(res.Body)
-	checkErr(queryErr)
-
-	doc.Find(".pagination").Each(func(i int, selection *goquery.Selection) {
-		pages = selection.Find("a").Length()
-	})
-
-	return pages
-}
-
-func checkCode(res *http.Response) {
-	if res.StatusCode != 200 {
-		log.Fatalln("Request failed with Status:", res.StatusCode)
-	}
-}
-
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func cleanString(str string) string {
-	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+func handleScrape(c echo.Context) error {
+	defer func() {
+		time.Sleep(time.Second * 2)
+		err := os.Remove(fileName)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
 }
